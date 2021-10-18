b97f998825094413cb6b93ca1316f71c1bbadcfa
tarrinneal <tarrinneal@gmail.com>
2021-10-17 22:49:32 -0700

    added margin to bottom

--- public/app-testimonials.js
+++ public/app-testimonials.js
@@ -9,7 +9,14 @@ import { containerStyles } from './container-styles.js';
 export class AppTestimonials extends LitElement {
   static get styles() {
     return window.innerWidth < 1190
-      ? containerStyles
+      ? [
+          containerStyles,
+          css`
+            .testimonialsCont {
+              margin-bottom: 20px;
+            }
+          `,
+        ]
       : [
           containerStyles,
           css`
