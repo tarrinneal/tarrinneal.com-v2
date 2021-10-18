70eb91de905113e2755b1228846be3893a0e3833
tarrinneal <tarrinneal@gmail.com>
2021-10-17 19:03:34 -0700

    added img or video for each project

--- public/app-projects.js
+++ public/app-projects.js
@@ -29,6 +29,8 @@ export class AppProjects extends LitElement {
       <p class="projects content">
         <a href="https://listf.in/" target="_blank">Listf.in</a> - Lead Architect and Engineer | <a href="https://github.com/listf-in/listf.in" target="_blank">GitHub</a>
         <br/>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/onqbkaO-hfU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <br/>
         Web-based, Kanban-style, list-making application with intuitive nested navigation structure
         <br/>
         ● Designed system architecture for full-stack app using React, Express, DGraph and Auth0
@@ -39,6 +41,8 @@ export class AppProjects extends LitElement {
         <br/>
         <br/>
         <a href="https://pacificmicroph.one/" target="_blank">Pacific Microphone</a> - Technical Lead and System Design | <a href="https://github.com/tarrinneal/Blue_Ocean_Pacific" target="_blank">GitHub</a>
+        <br/>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/0S4jHKTZhTE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <br/>
         Custom app for actors to practice and understand sentiment of scripts
         <br/>
@@ -54,6 +58,8 @@ export class AppProjects extends LitElement {
         <br/>
         <a href="https://fec-hopper.herokuapp.com/" target="_blank">HOPR</a> - Front-End Engineer | <a href="https://github.com/tarrinneal/hopper" target="_blank">GitHub</a>
         <br/>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/jBXznpJm4E4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <br/>
         Complete modern redesign of a client-facing retail web-portal utilizing legacy API<br/>
         ● Created custom image carousel using React and CSS3 to allow for responsive product navigation<br/>
         ● Designed adaptive web page layout using CSS Grid to precisely place elements on page in line
@@ -62,6 +68,14 @@ export class AppProjects extends LitElement {
         interface for purchasing products<br/><br/>
         Lily Pad - Backend Engineer | <a href="https://github.com/tarrinneal/hopper" target="_blank">GitHub</a>
         <br/>
+        <img
+          src="assets/backend.jpg"
+          alt="Lily Pad Scalability testing results"
+          width="560"
+          class="lilypadimg"
+        />
+        <br/>
+
         Overhaul of outdated API and database system to increase throughput from 400 to 4000 rps<br/>
         ● Created custom ETL tool to restructure data from csv files into query first Cassandra schema<br/>
         ● Reduced latency by 50% for frequently queried data with Redis cache<br/>
