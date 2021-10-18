4eab2a309acd71e12fc0396c4685b35898d75689
tarrinneal <tarrinneal@gmail.com>
2021-10-18 14:11:39 -0700

    h4 for project titles

--- public/app-projects.js
+++ public/app-projects.js
@@ -30,7 +30,7 @@ export class AppProjects extends LitElement {
     <div class="container projectsCont">
       <h2 id="projects" class="projects title">Projects</h1>
       <p class="projects content">
-        <a href="https://listf.in/" target="_blank">Listf.in</a> - Lead Architect and Engineer | <a href="https://github.com/listf-in/listf.in" target="_blank">GitHub</a>
+        <h4><a href="https://listf.in/" target="_blank">Listf.in</a></h4>Lead Architect and Engineer | <a href="https://github.com/listf-in/listf.in" target="_blank">GitHub</a>
         <br/>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/onqbkaO-hfU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <br/>
@@ -43,7 +43,8 @@ export class AppProjects extends LitElement {
         ● Lead team of 10+ engineers on open source project with agile methodologies while fully remote
         <br/>
         <br/>
-        <a href="https://pacificmicroph.one/" target="_blank">PacificMicroph.one</a> - Technical Lead and System Design | <a href="https://github.com/tarrinneal/Blue_Ocean_Pacific" target="_blank">GitHub</a>
+        <h4><a href="https://pacificmicroph.one/" target="_blank">PacificMicroph.one</a> </h4>
+        Technical Lead and System Design | <a href="https://github.com/tarrinneal/Blue_Ocean_Pacific" target="_blank">GitHub</a>
         <br/>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/0S4jHKTZhTE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <br/>
@@ -59,7 +60,8 @@ export class AppProjects extends LitElement {
         emotion/sentiment analysis via IBM’s Watson and WebEmpath APIs
         <br/>
         <br/>
-        <a href="https://fec-hopper.herokuapp.com/" target="_blank">HOPR</a> - Front-End Engineer | <a href="https://github.com/tarrinneal/hopper" target="_blank">GitHub</a>
+        <h4><a href="https://fec-hopper.herokuapp.com/" target="_blank">HOPR</a> - Front-End Engineer</h4>
+        <a href="https://github.com/tarrinneal/hopper" target="_blank">GitHub</a>
         <br/>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/jBXznpJm4E4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <br/>
@@ -69,7 +71,8 @@ export class AppProjects extends LitElement {
         with design document<br/>
         ● Managed data flow with Axios API calls and stateful React components to build an elegant user
         interface for purchasing products<br/><br/>
-        Lily Pad - Backend Engineer | <a href="https://github.com/tarrinneal/hopper" target="_blank">GitHub</a>
+        <h4>Lily Pad</h4>
+        Backend Engineer | <a href="https://github.com/tarrinneal/hopper" target="_blank">GitHub</a>
         <br/>
         <img
           src="assets/backend.jpg"
